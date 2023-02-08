f7b5dff67d2142d839669912aea8c8769ac3b558
qa-kcvdu-4-jszt@example.com <qa-kcvdu-4-jszt@example.com>
2023-02-08 16:30:37 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15190.stoplight-dev.com/api/v1/projects/cHJqOjE3NA/spectral.js?branch_slug=main&token=4bf6e609-53c8-41f0-a63d-8b5cf88535b2";
+export default { extends: ruleset };
